14f2ed358a31188c3d68ea17b74f77f5595453b1
JAAR <joe.aaron.acosta@outlook.com>
2023-07-16 00:36:31 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 07/16/2023
+>>> Practicing Fundamentals Program 7
+>>> Version 0
+'''
+
+'''
+>>> Creates a program that'll check if two sentences are palindrome and then counts the frequency of each word in both sentences.
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
